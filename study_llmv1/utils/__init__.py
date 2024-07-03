fdc4f941dbae7b1fe8390dc8012a8110f5b9f6a2
HyeongSeok Kim <tiryul@gmail.com>
2024-07-03 11:42:13 +0900

    update utils: encode_testing_example를 추가하다

--- study_llmv1/utils/__init__.py
+++ study_llmv1/utils/__init__.py
@@ -1,6 +1,7 @@
 from .apply_ocr import apply_ocr
 from .create_dataframe import create_dataframe
 from .encode_training_example import encode_training_example
+from .encode_testing_example import encode_testing_example
 from .label_mapping import label_mapping
 from .normalize_box import normalize_box
 from .training_dataloader_from_df import training_dataloader_from_df
